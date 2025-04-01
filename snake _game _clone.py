06eca03662b10869ce1896504ded9521255c790b
SowmisHub <vemulasowmika123@gmail.com>
2025-04-01 19:33:47 +0530

    snake _game _clone.py

--- snake _game _clone.py
+++ snake _game _clone.py
@@ -12,8 +12,9 @@ food_x, food_y = random.randrange(0,width)//10*10, random.randrange(0,height)//1
 clock = pygame.time.Clock()
 snake_body = [(snake_x,snake_y)]
 font = pygame.font.SysFont("arial", 40)
+score=0
 def display_snake_and_food():
-    global snake_x,snake_y,food_x,food_y
+    global snake_x,snake_y,food_x,food_y,score
     snake_x = (snake_x+change_x)%width
     snake_y = (snake_y+change_y)%height
     if ((snake_x, snake_y) in snake_body[1:]):
@@ -25,12 +26,15 @@ def display_snake_and_food():
     snake_body.append((snake_x,snake_y))
     if(food_x == snake_x and food_y == snake_y):
         food_x, food_y = random.randrange(0,width)//10*10, random.randrange(0,height)//10*10
+        score += 1
     else:
         del snake_body[0]
     game_screen.fill((0,0,0))
     pygame.draw.rect(game_screen,(0,255,0),[food_x,food_y,10,10])
     for (x,y) in snake_body:
         pygame.draw.rect(game_screen,(255,255,255),[x,y,10,10])
+    score_text = font.render(f"Score:{score}", True, (255, 255, 255))
+    game_screen.blit(score_text, (10, 10))
     pygame.display.update()
 while True:
     events=pygame.event.get()
